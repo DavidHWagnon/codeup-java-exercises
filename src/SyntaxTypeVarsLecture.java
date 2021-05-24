9c23e28fe364dfd0d9198c5ac4341446b0d41cf1
David Wagnon <david.wagnon02@gmail.com>
2021-05-24 14:17:10 -0500

    Completed Java Syntax, Types and Variables Lecture and exercises. Began Console IO Lecture

---
+++ src/SyntaxTypeVarsLecture.java
@@ -0,0 +1,78 @@
+public class SyntaxTypeVarsLecture {
+    public static void main(String[] args) {
+        System.out.println("Hello Douglas");
+        // Oh look you can use brackets to make lil comments
+
+        /* Using multiple line comments
+        right here
+        right now
+    * */
+
+        System.out.println("But I'm not ... :(");
+
+        byte smallNumber = 34;
+        System.out.println(smallNumber);
+
+        short notAsAmallNumber = 30000;
+
+        int num = 4500000;
+
+        long postID = 14;
+
+        System.out.println(postID);
+
+        float decimal = 1.5f;
+
+        double bigDecimal = 45;
+        System.out.println(bigDecimal);
+
+        char singleLetter = 'g';
+
+        boolean amILearningJava = true;
+
+        String message = "How's it going\nEverything \"alright\n\tJava isn't that scary, is it?";
+        System.out.println(message);
+
+        int sum = 4+5;
+        System.out.println(sum);
+
+        int difference = 72 - 19;
+        System.out.println(difference);
+
+        int product = 11 * 9;
+        System.out.println(product);
+
+        int quotient = product / sum;
+        System.out.println(quotient);
+
+        int remainder = difference % sum;
+        System.out.println(remainder);
+
+        int whatWillThisBe = -4 * 2;
+        System.out.println(whatWillThisBe);
+
+        int iAmAnInt = 44;
+        long iAmALong = iAmAnInt;
+        System.out.println(iAmALong);
+
+        iAmALong = 99;
+        iAmALong = (int) iAmALong;
+        System.out.println(iAmAnInt);
+
+        iAmAnInt = 27;
+        double iAmADouble = iAmAnInt;
+        System.out.println(iAmADouble);
+
+        iAmADouble = 3.1415926;
+        iAmAnInt = (int) iAmADouble;
+        System.out.println(iAmAnInt);
+
+
+
+
+    }
+
+
+
+
+}
