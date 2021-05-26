5dfcbfc9127fea692ad1f694c62603220a914981
David Wagnon <david.wagnon02@gmail.com>
2021-05-26 11:11:45 -0500

    Completed String Basics Exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -2,7 +2,15 @@ public class StringExercise {
     public static void main(String[] args) {
         String education = "We don't need no education\n" +
                 "We don't need no thought control";
+        System.out.println(education);
 
         String checkOut = "Check \"this\" out!, \"s inside of \"s!\n";
+        System.out.println(checkOut);
+
+        String windows = "In windows, the main drive is usually C:\\\n";
+        System.out.println(windows);
+
+        String backslash = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!\n";
     }
 }
