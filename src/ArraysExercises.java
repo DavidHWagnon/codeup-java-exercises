941b7d4f08909b4ddbbb1d7b47a0c9cee6015fb8
David Wagnon <david.wagnon02@gmail.com>
2021-06-04 09:13:54 -0500

    Completed part of the Arrays Exercise

---
+++ src/ArraysExercises.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class ArraysExercises {
+
+    public static Person[] addPerson(Person[] array, Person newPerson){
+        Person[] people = Arrays.copyOf(array, array.length + 1);
+        people[array.length] = newPerson;
+        return people;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+        Person[] people = new Person[3];
+        people[0] = new Person("Bulbasaur");
+        people[1] = new Person("Charmander");
+        people[2] = new Person("Squirtle");
+        for (Person person : people){
+            System.out.println(person.getName());
+        }
+        Person sophia = new Person("Sophia");
+        people = addPerson(people. sophia);
+        for (Person person : people){
+            System.out.println(person.getName());
+        }
+
+    }
+}
