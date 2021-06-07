bdf324e23f75c637dc8bbd84e8cb0d81f05d0ac2
David Wagnon <david.wagnon02@gmail.com>
2021-06-07 00:10:34 -0500

    Realised that Square.java was not correctly edited, refactored code and Interfaces and Abstract Exercises is now complete

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -6,6 +6,10 @@ public class ShapesTest {
         System.out.println(myShape.getPerimeter());
         System.out.println(myShape.getArea());
 
+        Measurable mySquare = new Square(5);
+        System.out.println(mySquare.getPerimeter());
+        System.out.println(mySquare.getArea());
+
 //        Rectangle box2 = new Square(5);
 //        System.out.println(box2.getPerimeter());
 //        System.out.println(box2.getArea());
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,22 +1,21 @@
 package shapes;
 
-public class Square extends Rectangle{
-//    double side;
-//    public Square(double side){
-//        super();
-//        this.side = side;
-//        length = side;
-//        width = side;
-//    }
-//    @Override
-//    public double getArea(){
-//        System.out.println(side);
-//        return side * side;
-//    }
-//    @Override
-//    public double getPerimeter(){
-//        return side * 4;
-//    }
+public class Square extends Shape.Quadrilateral implements Measurable{
+    double side;
+    public Square(double side){
+        super();
+        this.side = side;
+        length = side;
+        width = side;
+    }
+    @Override
+    public double getArea(){
+        return side * side;
+    }
+    @Override
+    public double getPerimeter(){
+        return side * 4;
+    }
 
 
 }
