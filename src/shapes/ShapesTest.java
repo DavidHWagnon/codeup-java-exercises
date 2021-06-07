1ffd4c5a19e967d312fb0486bd35d106840636bd
David Wagnon <david.wagnon02@gmail.com>
2021-06-07 00:06:45 -0500

    Changed box1 to myShape

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,9 +2,9 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
-        Measurable box1 = new Rectangle(6,6);
-        System.out.println(box1.getPerimeter());
-        System.out.println(box1.getArea());
+        Measurable myShape = new Rectangle(6,6);
+        System.out.println(myShape.getPerimeter());
+        System.out.println(myShape.getArea());
 
 //        Rectangle box2 = new Square(5);
 //        System.out.println(box2.getPerimeter());
